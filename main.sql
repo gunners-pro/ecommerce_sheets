2ff051d4098429d5bf8b8342b2d88634795fdbab
Fabricyo Barreto <bass.barreto@gmail.com>
2023-07-13 20:17:14 -0300

    feat: create table client

--- main.sql
+++ main.sql
@@ -1,3 +1,15 @@
 -- Criação do banco de dados para o cenário de E-commerce
 create database ecommerce;
 use ecommerce;
+
+-- criar tabela cliente
+
+create table client(
+	idClient int auto_increment primary key,
+    Fname varchar(10),
+    Minit char(3),
+    Lname varchar(20),
+    CPF char(11) not null,
+    Address varchar(30),
+    constraint unique_cpf_client unique (CPF)
+);
